362ae966e5c8d64cca92ddc87f441792c9cd45a5
num0005 <double.null@outlook.com>
2017-11-01 18:15:09 +0000

    Add icon.

--- Launcher/Properties/AssemblyInfo.cs
+++ Launcher/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Halo2CodezLauncher")]
+[assembly: AssemblyTitle("Halo 2 Codez Launcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Halo2CodezLauncher")]
+[assembly: AssemblyProduct("Halo 2 Codez Launcher")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
